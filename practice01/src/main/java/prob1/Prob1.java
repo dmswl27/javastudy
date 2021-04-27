501dbe44d600778c239ad4c2d5a54ab336706966
eunji <eunji@DESKTOP-07M5EDJ>
2021-04-27 12:38:54 +0900

    practice01.1

--- practice01/src/main/java/prob1/Prob1.java
+++ practice01/src/main/java/prob1/Prob1.java
@@ -7,7 +7,9 @@ public class Prob1 {
 		Scanner scanner = new Scanner( System.in );
 		
 		/* 코드 작성 */
-		
+		int num = scanner.nextInt();
+		if(num/3==0)
+			System.out.println("3의 배수입니다.");
 		scanner.close();
 	}
 }
