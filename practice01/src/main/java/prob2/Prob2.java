73b5fc9f832f95d118febc7ebad41936226e013f
eunji <eunji@DESKTOP-07M5EDJ>
2021-04-27 12:34:37 +0900

    practice01.2

--- practice01/src/main/java/prob2/Prob2.java
+++ practice01/src/main/java/prob2/Prob2.java
@@ -3,5 +3,11 @@ package prob2;
 public class Prob2 {
 	public static void main(String[] args) {
 		/* 코드 작성 */
+		
+		for(int i=1;i<9;i++) {
+			for(int j = 0;j<10;j++) {
+				System.out.println(i+j);
+			}
+		}
 	}
 }
